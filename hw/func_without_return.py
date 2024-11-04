5c088dee789f4dd5b28d701b2865cb1b4b3d0525
Shani Staretz <shakimi88@gmail.com>
2024-11-04 22:51:43 +0200

    complete training

--- hw/func_without_return.py
+++ hw/func_without_return.py
@@ -6,10 +6,12 @@ def my_ascending(number1: int, number2: int) -> None:
     """ accepts 2 int parameters and prints the numbers in ascending order."""
     print(sorted([number1, number2]))
 
+
 # ex b:
 def my_details(str1: str) -> None:
     """ accepts a string str and prints the first character of the string, the middle character, and the last character."""
     print(str1[0], str1[len(str1) // 2], str1[len(str1) - 1])
+
 
 # ex c:
 def say_bool(bool1: bool) -> None:
@@ -17,12 +19,14 @@ def say_bool(bool1: bool) -> None:
 Otherwise it will print "no"."""
     print(f"{"yes" if bool1 else "no"}")
 
+
 # ex d:
 def print_zugi(l1: list[int]) -> None:
     """accepts as a parameter a list of numbers and prints for each number in the list "even" or "odd"."""
     for num in l1:
         print(f"{"even" if num % 2 == 0 else "odd"}", end=" ")
     print()
+
 
 # ex e:
 def my_statistics(grades: list[int]) -> None:
